d48c3b4a3b51f59f9cbaad5879624ab039b32716
Stigmond <stigmond@gmail.com>
2020-09-04 21:58:01 -0600

    Added setters and getters to FoodTruck class

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -20,7 +20,50 @@ public class FoodTruck {
 		this.truckRating = truckRating;
 		this.truckId = ID_COUNTER;
 		ID_COUNTER++;
-		
+
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder truckinfo = new StringBuilder();
+		truckinfo.append("Truck ID: ")
+		.append(this.truckId)
+		.append(", Truck Name: ")
+		.append(this.truckName)
+		.append(", Food Type: ")
+		.append(this.foodType)
+		.append(", Truck Rating: ")
+		.append(this.truckRating);
+		return truckinfo.toString();
+	}
+	
+	public String getTruckName() {
+		return this.truckName;
+	}
+	
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+	
+	public String getFoodType() {
+		return this.foodType;
+	}
+	
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	
+	public int getTruckRating() {
+		return this.truckRating;
+	}
+	
+	public void setTruckRating(int truckRating) {
+		this.truckRating = truckRating;
+	}
+	
+	public int getTruckId() {
+		return this.truckId;
 	}
 	
 }
+
--- src/com/skilldistillery/foodproject/FoodTruckIgnition.java
+++ src/com/skilldistillery/foodproject/FoodTruckIgnition.java
@@ -1,23 +1,28 @@
 package com.skilldistillery.foodproject;
 
+import java.util.Scanner;
+
 public class FoodTruckIgnition {
 
 	public static void main(String[] args) {
 		
-		FoodTruckIgnition truckTest = new FoodTruckIgnition();
-		truckTest.start();
+		FoodTruckIgnition ft1 = new FoodTruckIgnition();
+		ft1.go();
 		
 	}
 
-	public void start() {
-		
-		FoodTruck truck1 = new FoodTruck("DogHaus", "Hot Dogs", 1);
-		FoodTruck truck2 = new FoodTruck("Pizza Man", "Pizza", 2);
-		FoodTruck truck3 = new FoodTruck();
-		
-		System.out.println(truck1.truckName + " - ID NUM: " + truck1.truckId);
-		System.out.println(truck2.truckName + " - ID NUM: " + truck2.truckId);
-		System.out.println(truck3.truckName + " - ID NUM: " + truck3.truckId);
+	public void go() {
+	
+//		FoodTruck truck1 = new FoodTruck("Pizza Man", "Pizza", 1);
+//		FoodTruck truck2 = new FoodTruck("DogHaus", "Sausages", 2);
+//		FoodTruck truck3 = new FoodTruck();
+//
+//		System.out.println(truck1.toString());
+//		System.out.println(truck2.toString());
+//		System.out.println(truck3.toString());
 	}
 
+	
+	
+	
 }
