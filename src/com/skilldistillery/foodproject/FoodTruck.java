be124105203fcdc804780f14b74ef9a922c43163
Stigmond <stigmond@gmail.com>
2020-09-05 02:58:06 -0600

    Completed creation of food truck array and user input to fill in food truck values

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -26,44 +26,39 @@ public class FoodTruck {
 	@Override
 	public String toString() {
 		StringBuilder truckinfo = new StringBuilder();
-		truckinfo.append("Truck ID: ")
-		.append(this.truckId)
-		.append(", Truck Name: ")
-		.append(this.truckName)
-		.append(", Food Type: ")
-		.append(this.foodType)
-		.append(", Truck Rating: ")
-		.append(this.truckRating);
+		
+		truckinfo.append("Truck ID: ").append(this.truckId).append(", Truck Name: ").append(this.truckName)
+				.append(", Food Type: ").append(this.foodType).append(", Truck Rating: ").append(this.truckRating);
+		
 		return truckinfo.toString();
 	}
-	
+
 	public String getTruckName() {
 		return this.truckName;
 	}
-	
+
 	public void setTruckName(String truckName) {
 		this.truckName = truckName;
 	}
-	
+
 	public String getFoodType() {
 		return this.foodType;
 	}
-	
+
 	public void setFoodType(String foodType) {
 		this.foodType = foodType;
 	}
-	
+
 	public int getTruckRating() {
 		return this.truckRating;
 	}
-	
+
 	public void setTruckRating(int truckRating) {
 		this.truckRating = truckRating;
 	}
-	
+
 	public int getTruckId() {
 		return this.truckId;
 	}
-	
-}
 
+}
--- src/com/skilldistillery/foodproject/FoodTruckIgnition.java
+++ src/com/skilldistillery/foodproject/FoodTruckIgnition.java
@@ -5,24 +5,67 @@ import java.util.Scanner;
 public class FoodTruckIgnition {
 
 	public static void main(String[] args) {
-		
+
 		FoodTruckIgnition ft1 = new FoodTruckIgnition();
 		ft1.go();
-		
+
 	}
 
 	public void go() {
-	
-//		FoodTruck truck1 = new FoodTruck("Pizza Man", "Pizza", 1);
-//		FoodTruck truck2 = new FoodTruck("DogHaus", "Sausages", 2);
-//		FoodTruck truck3 = new FoodTruck();
-//
-//		System.out.println(truck1.toString());
-//		System.out.println(truck2.toString());
-//		System.out.println(truck3.toString());
+
+		int truckMax = 5;
+		// Make Array with default Food Trucks;
+		FoodTruck[] foodTruckArray = new FoodTruck[truckMax];
+		foodTruckArray = makeArray(truckMax);
+		// Fill data of the default Food Trucks
+		foodTruckArray = makeFoodTrucks(foodTruckArray, truckMax);
+
+		// 	Test code to make sure the array has been filled according to instructions.
+				System.out.println(foodTruckArray[0].toString());
+				System.out.println(foodTruckArray[1].toString());
+				System.out.println(foodTruckArray[2].toString());
+				System.out.println(foodTruckArray[3].toString());
+				System.out.println(foodTruckArray[4].toString());
 	}
 
 	
+	public FoodTruck[] makeArray(int truckMax) {
+		FoodTruck[] returnArray = new FoodTruck[truckMax];
+		for (int i = 0; i < truckMax; i++) {
+			FoodTruck emptyTruck = new FoodTruck();
+			returnArray[i] = emptyTruck;
+		}
+		return returnArray;
+	}
+
+	
+	public FoodTruck[] makeFoodTrucks(FoodTruck[] tempArray, int truckMax) {
+
+		Scanner input = new Scanner(System.in);
+
+		FoodTruck[] returnArray = tempArray;
+
+		System.out.println("Please enter food truck info:");
+
+		for (int i = 0; i < truckMax; i++) {
+
+			System.out.print("Truck No. " + (i + 1) + " Name (or type 'quit' if finished): ");
+			tempArray[i].setTruckName(input.nextLine());
+			if (tempArray[i].getTruckName().equals("quit")) {
+				break;
+			}
+			System.out.print("Truck No. " + (i + 1) + " Food Type(s): ");
+			tempArray[i].setFoodType(input.nextLine());
+			System.out.print("Truck No. " + (i + 1) + " Rating: ");
+			tempArray[i].setTruckRating(input.nextInt());
+			input.nextLine();
+		}
+
+		input.close();
+		return returnArray;
+	
+	}
 	
 	
 }
+
